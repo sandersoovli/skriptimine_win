34e1ac95c63e251059cba6da0abc5171216d00dd
SanderSoovali <sander.soovali@voco.ee>
2022-11-09 11:19:15 +0200

    lokaalne\ kasutamine_parameetrid.ps1

---
+++ lokaalne kasutamine_parameetrid.ps1
@@ -0,0 +1,10 @@
+﻿# vajalikud parameetrid
+param(
+$KastajaNimi,
+$TaisNimi,
+$KontoKirjeldus
+)
+#loome kasutaja jaoks parool
+$KasutajaParool = ConvertTo-SecureString 'Qwerty12!' -AsPlainText -Force
+# loome kasutaja
+New-LocalUser $KastajaNimi -Password $KasutajaParool -FullName 'TaisNimi' -Description 'KontoKirjeldus'
