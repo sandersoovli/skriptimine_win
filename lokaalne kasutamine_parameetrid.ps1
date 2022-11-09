6c18f566ed98f9d8dd547b1497fbfeb15238fab3
SanderSoovali <sander.soovali@voco.ee>
2022-11-09 11:49:00 +0200

    lokaalne\ kasutamine_parameetrid.ps1

--- lokaalne kasutamine_parameetrid.ps1
+++ lokaalne kasutamine_parameetrid.ps1
@@ -1,10 +1,13 @@
-﻿# vajalikud parameetrid
-param(
-$KastajaNimi,
-$TaisNimi,
-$KontoKirjeldus
-)
-#loome kasutaja jaoks parool
+﻿# kontroll parameetrite arvule
+if($args.Count -ne 3){
+    echo ".\skriptifailnimi.ps1 kasutajanimi täisnimi kirjeldus"
+} else{
+    # vajalikud parameetrid
+    $KastajaNimi = $args[0]
+    $TaisNimi = $args[1]
+    $KontoKirjeldus = $args[2]
+    #loome kasutaja jaoks parool
 $KasutajaParool = ConvertTo-SecureString 'Qwerty12!' -AsPlainText -Force
-# loome kasutaja
+    # loome kasutaja
 New-LocalUser $KastajaNimi -Password $KasutajaParool -FullName 'TaisNimi' -Description 'KontoKirjeldus'
+}
