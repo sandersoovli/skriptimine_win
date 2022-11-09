b81801e0cefbfb246e42f020b684f022b187852f
SanderSoovali <sander.soovali@voco.ee>
2022-11-09 10:43:42 +0200

    lokaalne kasutamine.ps1

---
+++ lokaalne kasutamine.ps1
@@ -0,0 +1,4 @@
+﻿# loome kasutaja jaoks parool
+$KasutajaParool = ConvertTo-SecureString 'Qwerty12!' -AsPlainText -Force
+# loome kasutaja
+New-LocalUser 'kasutaja1' -Password $KasutajaParool -FullName 'Esimene kasutaja' -Description 'local account'
