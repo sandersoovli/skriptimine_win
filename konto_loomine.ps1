b0f4435f103db1584c34b20b71a562b2c983e2b1
SanderSoovali <sander.soovali@voco.ee>
2022-11-14 11:52:03 +0200

    konto_loomine.ps1

---
+++ konto_loomine.ps1
@@ -0,0 +1,12 @@
+﻿# loome_kasutaja_ja_parool
+$kasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+# küsime kasutaja andmeds
+$Eesnimi = Read-Host "Sisesta oma Eesnimi"
+$Perenimi = Read-Host "Sisesta oma Perenimi"
+# loome_kasutajanimi
+$kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+# taisnime loomine
+$Taisnimi = $Eesnimi + "." + $Perenimi
+# kontokirjeldus
+$kasutajakirjeldus = "Lokaalne_konto -" + $kasutajanimi
+New-LocalUser $kasutajanimi -Password $kasutajaParool -FullName $Taisnimi -Description $kasutajakirjeldus
