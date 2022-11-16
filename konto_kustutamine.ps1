0e230f23be23be3a2303d82adb3a5ac8c6cedb39
SanderSoovali <sander.soovali@voco.ee>
2022-11-16 17:06:50 +0200

    konto_kustutamine.ps1

---
+++ konto_kustutamine.ps1
@@ -0,0 +1,17 @@
+﻿#kustutab kasutaja ees ja perenime järgi
+$Eesnimi = Read-Host "Sisesta oma Eesnimi"
+$Perenimi = Read-Host "Sisesta oma Perenimi"
+#kasutajanimi
+$kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+$ErrorActionPreference = "SilentlyContinue"
+#konto_kustutamine
+Remove-LocalUser  $kasutajanimi
+if(!$?){
+
+Echo "Viga! kontot ei eksisteeri."
+}
+else
+{
+Echo "Konto $kasutajanimi on kustutatud"
+}
+$ErrorActionPreference = "Stop"
