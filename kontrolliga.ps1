cd2ad93977df17c5154183ae179bab88575e6483
SanderSoovali <sander.soovali@voco.ee>
2022-11-16 16:05:21 +0200

    kontrolliga.ps1

---
+++ kontrolliga.ps1
@@ -0,0 +1,18 @@
+﻿# loome_kasutaja_ja_parool
+$kasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+# küsime kasutaja andmeds
+$Eesnimi = Read-Host "Sisesta oma Eesnimi"
+$Perenimi = Read-Host "Sisesta oma Perenimi"
+# loome_kasutajanimi
+$kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+# taisnime loomine
+$Taisnimi = $Eesnimi + "." + $Perenimi
+# kontokirjeldus
+$kasutajakirjeldus = "Lokaalne_konto -" + $kasutajanimi
+New-LocalUser $kasutajanimi -Password $kasutajaParool -FullName $Taisnimi -Description $kasutajakirjeldus
+# kontroll
+if(!$?){
+    Write-output "Tekkinud probleem kasutaja loomisega"
+}
+# süsteemi teaded tagasi sisse
+$ErrorActionPreference = 'stop'
